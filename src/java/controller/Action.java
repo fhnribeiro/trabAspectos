2d3d6879d6b4ebcbdae9d53c3b1fc70c92dcc5c6
Igor Couto <igor.fcouto@gmail.com>
2017-09-28 14:47:09 -0300

    Criado o package Action e remoção de comentários inúteis
    Criado o package Action com algumas ações.

--- src/java/controller/Action.java
+++ src/java/controller/Action.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// Teste
-// TESTE!
-
 public interface Action {
     public void execute ( HttpServletRequest request, HttpServletResponse response ) throws IOException;
 }

dc213f859260888eb9b11965f4c9734ef6aad917
Igor Couto <igor.fcouto@gmail.com>
2017-10-02 02:35:42 -0300

    Criado o campo senha

--- src/java/model/Usuario.java
+++ src/java/model/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario {
     }
 
     public Usuario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     public int getId() {
